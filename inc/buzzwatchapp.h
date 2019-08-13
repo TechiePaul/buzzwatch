27726dc1ecdbc0d4c429e48543b78e556fdbcc28
TechiePaul <54035532+TechiePaul@users.noreply.github.com>
2019-08-13 08:03:31 +0100

    Initial checkin
    This template for the buzz watch project, currently running hello world. Confirmed to operate on the Gear Sport watch and the V3.0 emulator.

---
+++ inc/buzzwatchapp.h
@@ -0,0 +1,19 @@
+#ifndef __buzzwatchapp_H__
+#define __buzzwatchapp_H__
+
+#include <app.h>
+#include <Elementary.h>
+#include <system_settings.h>
+#include <efl_extension.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "buzzwatchapp"
+
+#if !defined(PACKAGE)
+#define PACKAGE "org.example.buzzwatchapp"
+#endif
+
+#endif /* __buzzwatchapp_H__ */
